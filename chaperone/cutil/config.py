d5e0ce027d276ad54841ae9250caa2087b43919c
loganwoodxyz <71326821+loganwoodxyz@users.noreply.github.com>
2021-11-21 21:31:52 -0600

    Update config.py

--- chaperone/cutil/config.py
+++ chaperone/cutil/config.py
@@ -512,7 +512,7 @@ class Configuration(object):
 
         for fn in args:
             if os.path.exists(fn):
-                self._merge(yaml.load(open(fn, 'r').read().expandtabs()))
+                self._merge(yaml.safe_load(open(fn, 'r').read().expandtabs()))
         
         if not self._conf and default:
             self._conf = lazydict(yaml.load(default))
